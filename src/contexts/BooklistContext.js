91c538169cd885f99113f78f0feadc9a7c04f35e
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-04-09 14:56:05 +0600

    implemented useReducer hook

--- src/contexts/BooklistContext.js
+++ src/contexts/BooklistContext.js
@@ -1,20 +1,13 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BooklistContext = createContext();
 
 const BooklistContextProvider = (props) => {
-  const [books, setBooks] = React.useState([
-    { title: "The fault in our stars", author: "John Green", id: 1 },
-    { title: "Looking for Alaska", author: "John Green", id: 2 },
-  ]);
-  const addBooks = (title, author) => {
-    setBooks([...books, { title, author, id: books.length + 1 }]);
-  };
-  const removeBooks = (id) => {
-    setBooks(books.filter((book) => book.id !== id));
-  };
+  const [books, dispatch] = useReducer(bookReducer, []);
+
   return (
-    <BooklistContext.Provider value={{ books, addBooks, removeBooks }}>
+    <BooklistContext.Provider value={{ books, dispatch }}>
       {" "}
       {props.children}{" "}
     </BooklistContext.Provider>
